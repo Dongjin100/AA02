ec4eb6a1c57fe1424dfb96947caddca3f09c1d8c
One Hundred DJ <54879676+Dongjin100@users.noreply.github.com>
2019-09-14 23:50:08 +0900

    Add files via upload

---
+++ aa02info.js
@@ -0,0 +1,23 @@
+//aa02info.js
+module.exports.myinfo = function(id, name, phone){
+	console.log("my Info");
+	console.log("ID : "+id);
+	console.log("Name: "+name);
+	console.log("Phone :" + phone +"\n");
+
+}
+
+module.exports.jin = function(id, name, phone){
+	console.log("my Info");
+	console.log("ID : "+id);
+	console.log("Name: "+name);
+	console.log("Phone :" + phone +"\n");
+
+}
+module.exports.jon = function(id, name, phone){
+	console.log("my Info");
+	console.log("ID : "+id);
+	console.log("Name: "+name);
+	console.log("Phone :" + phone +"\n");
+
+}
---
+++ index_aa02.js
@@ -0,0 +1,5 @@
+//index_aa02.js
+var info = require('./aa02info'); 
+info.myinfo("aa02", "aa02@naver.com", "101-1234-5678");
+info.jin("aa01", "qorehdwls@naver.com", "101-1234-5678");
+info.jon("aa00", "dongjin100@naver.com", "101-1234-5678");
